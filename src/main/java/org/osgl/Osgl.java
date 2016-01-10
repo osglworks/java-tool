970df98d9dd1bdef73538509185caa8bd5f93888
Gelin Luo <greenlaw110@gmail.com>
2016-01-11 08:49:22 +1100

    move asMap from T2 to Tuple, add asMap(List<Tuple>) static method to Tuple

--- src/main/java/org/osgl/Osgl.java
+++ src/main/java/org/osgl/Osgl.java
@@ -2591,6 +2591,38 @@ public class Osgl implements Serializable {
         public String toString() {
             return "T2(_1: " + _1 + ", _2: " + _2 + ")";
         }
+
+        /**
+         * Convert this {@code Tuple} instance into a Map with one key,value pair. Where
+         * {@code key} is {@code _1} and {@code value} is {@code _2};
+         * @return the map as described
+         */
+        @SuppressWarnings("unused")
+        public Map<A, B> asMap() {
+            Map<A, B> m = new HashMap<A, B>();
+            m.put(_1, _2);
+            return m;
+        }
+
+        /**
+         * Convert a list of {@code Tuple} instances into a Map. Where
+         * {@code key} is {@code _1} and {@code value} is {@code _2};
+         * <p>
+         *     <b>Note</b> that the size of the returned map might be lesser than
+         *     the size of the tuple list if there are multiple {@code _1} has
+         *     the same value, and the last one is the winner and it's {@code _2}
+         *     will be put into the map
+         * </p>
+         * @return the map as described
+         */
+        @SuppressWarnings("unused")
+        public static <K, V> Map<K, V> asMap(Collection<Tuple<K, V>> list) {
+            Map<K, V> m = C.newMap();
+            for (Tuple<K, V> t: list) {
+                m.put(t._1, t._2);
+            }
+            return m;
+        }
     }
 
     @SuppressWarnings("unused")
@@ -2604,12 +2636,6 @@ public class Osgl implements Serializable {
             super(_1, _2);
         }
 
-        @SuppressWarnings("unused")
-        public Map<A, B> asMap() {
-            Map<A, B> m = new HashMap<A, B>();
-            m.put(_1, _2);
-            return m;
-        }
     }
 
     public static <A, B> T2<A, B> T2(A a, B b) {
