4285730b861866b6b498acc7849b587adb0b8674
Gelin Luo <greenlaw110@gmail.com>
2016-08-12 06:44:49 +1000

    fix javadoc issue

--- src/main/java/org/osgl/util/StringValueResolver.java
+++ src/main/java/org/osgl/util/StringValueResolver.java
@@ -72,7 +72,7 @@ public abstract class StringValueResolver<T> extends $.F1<String, T> {
      * 1. if there value is null or empty length String then return `defval` specified
      * 2. if the length of the String is `1`, then return that one char in the string
      * 3. if the value not starts with '\', then throw `IllegalArgumentException`
-     * 4. if the value starts with `\u` then parse the integer using `16` radix. The check
+     * 4. if the value starts with `\\u` then parse the integer using `16` radix. The check
      *    the range, if it fall into Character range, then return that number, otherwise raise
      *    `IllegalArgumentException`
      * 5. if the value length is 2 then check if it one of {@link #PREDEFINED_CHARS}, if found
