22c7407b358b856fddca09a18e70234321ed7425
Gelin Luo <greenlaw110@gmail.com>
2017-03-07 12:34:50 +1100

    Add Generics.buildTypeParamImplLookup(Class) API

--- src/main/java/org/osgl/util/Generics.java
+++ src/main/java/org/osgl/util/Generics.java
@@ -2,11 +2,14 @@ package org.osgl.util;
 
 import org.osgl.$;
 
+import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Provides utilities for generic relevant operations
@@ -48,6 +51,57 @@ public class Generics {
         } catch (IndexOutOfBoundsException e) {
             throw new IllegalArgumentException(S.fmt("Cannot infer type parameter implementation on %s against %s", theClass.getName(), rootClass.getName()), e);
         }
+    }
+
+    /**
+     * Build class type variable name and type variable implementation lookup
+     * @param theClass the class to build the lookup
+     * @return the lookup
+     */
+    public static Map<String, Class> buildTypeParamImplLookup(Class theClass) {
+        Map<String, Class> lookup = new HashMap<>();
+        buildTypeParamImplLookup(theClass, lookup);
+        return lookup;
+    }
+
+    public static void buildTypeParamImplLookup(Class theClass, Map<String, Class> lookup) {
+        if (null == theClass || Object.class == theClass) {
+            return;
+        }
+
+        // what type variable of the super class implemented by this class
+        Type superType = theClass.getGenericSuperclass();
+        if (superType instanceof ParameterizedType) {
+            ParameterizedType ptype = $.cast(superType);
+            Type[] typeParams = ptype.getActualTypeArguments();
+            TypeVariable[] typeArgs = theClass.getSuperclass().getTypeParameters();
+            int len = typeParams.length;
+            for (int i = 0; i < len; ++i) {
+                Type typeParam = typeParams[i];
+                if (typeParam instanceof Class) {
+                    TypeVariable typeVar = typeArgs[i];
+                    lookup.put(typeVar.getName(), (Class) typeParam);
+                } else if (typeParam instanceof TypeVariable) {
+                    TypeVariable var = $.cast(typeParam);
+                    String name = var.getName();
+                    Class impl = lookup.get(name);
+                    TypeVariable typeVar = typeArgs[i];
+                    if (null != impl) {
+                        lookup.put(typeVar.getName(), impl);
+                    } else {
+                        Type[] ta = var.getBounds();
+                        if (null != ta && ta.length == 1) {
+                            Type bound = ta[0];
+                            if (bound instanceof Class) {
+                                lookup.put(typeVar.getName(), (Class) bound);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        buildTypeParamImplLookup(theClass.getSuperclass(), lookup);
     }
 
     private static List<Type> typeParamImplementations(Class theClass, Class rootClass, List<Type> subClassTypeParams) {
@@ -99,6 +153,12 @@ public class Generics {
             return typeParamImplementations(superClass, rootClass, nextList);
         }
         throw E.unexpected("Cannot find type param implementation: super type %s of %s is not a parameterized type", superType, theClass);
+    }
+
+    public static void main(String[] args) {
+        Class c = Serializable.class;
+        System.out.println(c.getGenericSuperclass());
+        System.out.println($.toString2(c.getGenericInterfaces()));
     }
 
 
