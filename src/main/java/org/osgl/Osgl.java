cde3944b52f0b00fcb617c325c918def675e1d08
Gelin Luo <greenlaw110@gmail.com>
2017-09-10 07:46:36 +1000

    fix #36 and work on #37

--- src/main/java/org/osgl/Osgl.java
+++ src/main/java/org/osgl/Osgl.java
@@ -1,5 +1,25 @@
 package org.osgl;
 
+/*-
+ * #%L
+ * Java Tool
+ * %%
+ * Copyright (C) 2014 - 2017 OSGL (Open Source General Library)
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import org.osgl.cache.CacheService;
 import org.osgl.concurrent.ContextLocal;
 import org.osgl.exception.*;
@@ -4651,9 +4671,9 @@ public class Osgl implements Serializable {
      * @return {@code true} if any one of the argument is {@code null}
      */
     public static boolean anyNull(Object o, Object... oa) {
-        if (!isNull(o)) return true;
+        if (isNull(o)) return true;
         for (int i = oa.length - 1; i >= 0; --i) {
-            if (!isNull(oa[i])) return true;
+            if (isNull(oa[i])) return true;
         }
         return false;
     }
--- src/test/java/org/osgl/OsglTest.java
+++ src/test/java/org/osgl/OsglTest.java
@@ -1,5 +1,25 @@
 package org.osgl;
 
+/*-
+ * #%L
+ * Java Tool
+ * %%
+ * Copyright (C) 2014 - 2017 OSGL (Open Source General Library)
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import org.junit.Test;
 import org.osgl.util.C;
 import org.osgl.util.S;
@@ -69,6 +89,14 @@ public class OsglTest extends TestBase {
 
         assertSame(Code.AB, $.asEnum(Code.class, "AB", true));
         assertNull($.asEnum(Code.class, "ab", true));
+    }
+
+    @Test
+    public void testAnyNull() {
+        yes($.anyNull(null));
+        yes($.anyNull(null, "a"));
+        yes($.anyNull("a", "b", null));
+        yes($.anyNull("a", null, 1));
     }
 
 }
