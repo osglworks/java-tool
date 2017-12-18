f4fd6cfdc9f9b8b078f4621c6ee5563cbe971310
Gelin Luo <greenlaw110@gmail.com>
2017-12-19 10:58:08 +1100

    big updates to pom.xml: apply osgl-parent

--- src/main/java/org/osgl/Osgl.java
+++ src/main/java/org/osgl/Osgl.java
@@ -1,9 +1,30 @@
 package org.osgl;
 
+/*-
+ * #%L
+ * Java Tool
+ * %%
+ * Copyright (C) 2014 - 2017 OSGL (Open Source General Library)
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import org.osgl.cache.CacheService;
 import org.osgl.concurrent.ContextLocal;
 import org.osgl.exception.*;
 import org.osgl.util.*;
+import osgl.version.Version;
 
 import java.io.*;
 import java.lang.annotation.Annotation;
@@ -65,6 +86,8 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @version 0.8
  */
 public class Osgl implements Serializable {
+
+    public static final Version VERSION = Version.get();
 
     public static final Osgl INSTANCE = new Osgl();
 
--- src/test/java/org/osgl/OsglTest.java
+++ src/test/java/org/osgl/OsglTest.java
@@ -1,5 +1,25 @@
 package org.osgl;
 
+/*-
+ * #%L
+ * Java Tool
+ * %%
+ * Copyright (C) 2014 - 2017 OSGL (Open Source General Library)
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import org.junit.Test;
 import org.osgl.util.C;
 import org.osgl.util.S;
@@ -49,11 +69,20 @@ public class OsglTest extends TestBase {
     @Test
     public void testFieldsOf() {
         List<Field> fields = $.fieldsOf(Bar.class, false);
-        eq(4, fields.size());
-        eq(4, new HashSet<Field>(fields).size());
+        int jacocoFields = 0;
+        if (4 != fields.size()) {
+            // we are running with jacoco enhancement
+            for (Field f : fields) {
+                if (f.getName().contains("jacoco")) {
+                    jacocoFields++;
+                }
+            }
+        }
+        eq(4 + jacocoFields, fields.size());
+        eq(4 + jacocoFields, new HashSet<>(fields).size());
         fields = $.fieldsOf(Bar.class, true);
         eq(3, fields.size());
-        eq(3, new HashSet<Field>(fields).size());
+        eq(3, new HashSet<>(fields).size());
     }
 
     enum Code {
